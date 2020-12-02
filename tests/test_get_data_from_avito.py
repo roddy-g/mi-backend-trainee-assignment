9526d1081b50544d22c9b8220193e674816b16d1
Roddy <per677199@yandex.ru>
2020-12-02 22:28:08 +0300

    test get data stat

--- tests/test_get_data_from_avito.py
+++ tests/test_get_data_from_avito.py
@@ -5,4 +5,4 @@ def test_get_data_from_avito():
     phrase = 'iphone'
     advert = schemas.Advert(phrase=phrase, location_id=moscow_location_id)
     assert get_data_from_avito.get_data_stat(advert) is not None
-    assert get_data_from_avito.get_data_stat(advert) != 'Not 200 status code'
+    assert type(get_data_from_avito.get_data_stat(advert)) != str
