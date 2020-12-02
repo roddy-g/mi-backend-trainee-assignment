5a1a757a7af8ee166acd094db3183e2c8d2b8e52
Roddy <per677199@yandex.ru>
2020-12-02 21:50:24 +0300

    update test

--- tests/test_get_data_from_avito.py
+++ tests/test_get_data_from_avito.py
@@ -2,5 +2,6 @@ from api import get_data_from_avito, schemas
 
 def test_get_data_from_avito():
     moscow_location_id = 637640
-    advert = schemas.Advert(phrase='iphone', location_id=moscow_location_id)
+    phrase = 'iphone'
+    advert = schemas.Advert(phrase=phrase, location_id=moscow_location_id)
     assert get_data_from_avito.get_data_stat(advert) is not None
