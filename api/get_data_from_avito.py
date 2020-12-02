f2d5df9ae099b98741c93f254065a1a782094952
Roddy <per677199@yandex.ru>
2020-12-02 22:18:13 +0300

    test get data stat

--- api/get_data_from_avito.py
+++ api/get_data_from_avito.py
@@ -10,7 +10,7 @@ def get_data_stat(advertise: schemas.Advert):
         .format(advertise.phrase, advertise.location_id)
     response = requests.get(url)
     if response.status_code != 200:
-        return 'Not 200 status code'
+        return 'status code'.format(response.status_code)
     data = response.json()
     try:
         advert_count = data['result']['totalCount']
