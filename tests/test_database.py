4a9ebc9c72cf3e82f6128313ac48c746bf333e68
Roddy <per677199@yandex.ru>
2020-12-05 13:14:41 +0300

    trying to fix tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -47,7 +47,8 @@ def test_add_stats():
 
 def test_get_item_stat():
     db = TestingSessionLocal()
-    assert database_functions.get_item_stat(db, test_item_stat_request) is None
+    database_functions.clear_db(db)
+    #assert database_functions.get_item_stat(db, test_item_stat_request) is None
     database_functions.add_item(db, test_item)
     for stat in item_stats:
         database_functions.add_stats(db, stat)
