1a25b78c073e82383a70b5a4031b4f6b97d43b6e
Roddy <per677199@yandex.ru>
2020-12-07 11:42:17 +0300

    add fixture "db"

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -7,7 +7,7 @@ from tests.fixtures.items_stats import *
 from tests.fixtures.item_stat_requests import *
 from tests.fixtures.responses_data import *
 from datetime import datetime, timedelta
-
+import pytest
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./tests/fixtures/test_db.db"
 engine = create_engine(
@@ -18,43 +18,38 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 Base.metadata.create_all(bind=engine)
 
 
-def test_get_item():
+@pytest.fixture
+def db():
     db = TestingSessionLocal()
+    yield db
+    storage.clear_db(db)
+    db.close()
+
+
+def test_get_item(db):
     storage.add_item(db, test_item)
     location_id = storage.get_item(db, test_item).location_id
     assert location_id == test_item.location_id
-    storage.clear_db(db)
-    db.close()
 
 
-def test_add_item():
-    db = TestingSessionLocal()
+def test_add_item(db):
     advert_id = storage.add_item(db, test_item).id
     assert advert_id == storage.get_item(db, test_item).id
-    storage.clear_db(db)
-    db.close()
 
 
-def test_add_stats():
-    db = TestingSessionLocal()
+def test_add_stats(db):
     storage.add_item(db, test_item)
     for stat in item_stats:
         storage.add_stats(db, stat)
     assert storage.get_item_stat(db, test_item_stat_request) == response_stat
-    storage.clear_db(db)
-    db.close()
 
 
-def test_get_item_stat():
-    db = TestingSessionLocal()
-    storage.clear_db(db)
+def test_get_item_stat(db):
     assert storage.get_item_stat(db, test_item_stat_request) is None
     storage.add_item(db, test_item)
     for stat in item_stats:
         storage.add_stats(db, stat)
     assert storage.get_item_stat(db, test_item_stat_request) == response_stat
-    storage.clear_db(db)
-    db.close()
 
 
 def test_get_date_some_days_ago():
@@ -64,18 +59,14 @@ def test_get_date_some_days_ago():
     assert storage.get_date_some_days_ago(days_delta).date() == date_delta_days_ago.date()
 
 
-def test_get_item_by_id():
-    db = TestingSessionLocal()
+def test_get_item_by_id(db):
     item_id = storage.add_item(db, test_item).id
     item_from_db = storage.get_item(db, test_item)
     assert item_from_db.phrase == test_item.phrase
     assert item_from_db.location_id == test_item.location_id
-    storage.clear_db(db)
-    db.close()
 
 
-def test_clear_db():
-    db = TestingSessionLocal()
+def test_clear_db(db):
     storage.add_item(db, test_item)
     storage.add_item(db, test_item_1)
     for stat in item_stats:
@@ -83,4 +74,4 @@ def test_clear_db():
     storage.clear_db(db)
     assert storage.get_item(db, test_item) is None
     assert storage.get_item_stat(db, test_item_stat_request) is None
-    db.close()
+
